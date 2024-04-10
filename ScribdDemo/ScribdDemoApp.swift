774ee7a53b8818cb79fd5b0efb2b6aec0435eb17
cedonia <sashadaisy1999@gmail.com>
2024-04-10 19:46:42 +0100

    Initial Commit

---
+++ ScribdDemo/ScribdDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScribdDemoApp.swift
+//  ScribdDemo
+//
+//  Created by Cedonia Peterson on 10/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScribdDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
